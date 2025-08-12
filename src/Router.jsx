0c7e5aaa5061bde17c785566f0eb1bf3d54e8541
mt9ing <mt9ing@gmail.com>
2025-08-12 14:11:22 +0300

    added about contact

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/header.component.jsx'
 import About from './pages/about.page.jsx'
+import Contact from './pages/contact.page.jsx'
 import Home from './pages/home.page'
 import NotFoundPage from './pages/notFound.page.jsx'
 
@@ -11,6 +12,7 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         {/* Add more routes as needed */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
---
+++ src/pages/contact.page.jsx
@@ -0,0 +1,99 @@
+import { LocationEdit, Mail } from 'lucide-react'
+
+import { SocialIcon } from 'react-social-icons'
+
+export default function Contact() {
+  return (
+    <div className="contact-container">
+      <div className="contact-header">
+        <h1>Contact Us</h1>
+        <p>If you have any questions or feedback, feel free to reach out!</p>
+      </div>
+      <form action="mailto:mmtcommunityguide@gmail.com">
+        <input type="text" placeholder="Your Name" required />
+        <input type="email" placeholder="Your Email" required />
+        <textarea placeholder="Your Message" rows={6} required></textarea>
+        <button type="submit">Send Message</button>
+      </form>
+      <div className="social-media">
+        <h2>Connect With Us</h2>
+        <p>Follow us on our social media channels for the latest updates!</p>
+        <ul>
+          <li>
+            <SocialIcon
+              url="https://www.facebook.com"
+              bgColor="white"
+              fgColor="black"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            <p>Facebook</p>
+          </li>
+          <li>
+            <SocialIcon
+              url="https://www.instagram.com"
+              bgColor="white"
+              fgColor="black"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            <p>Instagram</p>
+          </li>
+          <li>
+            <SocialIcon
+              url="https://www.linkedin.com"
+              bgColor="white"
+              fgColor="black"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            <p>LinkedIn</p>
+          </li>
+          <li>
+            <SocialIcon
+              url="https://www.youtube.com"
+              bgColor="white"
+              fgColor="black"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            <p>Youtube</p>
+          </li>
+        </ul>
+      </div>
+      <div className="contact-info">
+        <h2>Direct Contact</h2>
+        <p>
+          For specific inquiries, you can reach us directly via email or visit
+          our office
+        </p>
+        <div className="dc-info">
+          <a
+            href="http://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Mail />
+          </a>
+          <div>
+            <h3>Email</h3>
+            <p>mmtcommunityguide@gmail.com</p>
+          </div>
+        </div>
+        <div className="dc-info">
+          <a
+            href="http://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LocationEdit />
+          </a>
+          <div>
+            <h3>Address</h3>
+            <p>123 Main St, Anytown, USA</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
