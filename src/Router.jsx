64fb32f3dd19e2c66fdea610886e6c441fe11b7a
mt9ing <mt9ing@gmail.com>
2025-08-13 09:58:37 +0300

    added mobile preventer

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,6 +6,23 @@ import Home from './pages/home.page'
 import NotFoundPage from './pages/notFound.page.jsx'
 
 export default function Router() {
+  if (screen.width < 500) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '100dvh',
+          padding: '2rem',
+        }}
+      >
+        <h1>Hello, World!</h1>
+        <p>NOTE: "This site is only available on (PC\Laptop) devices."</p>
+      </div>
+    )
+  }
   return (
     <>
       <Header />
